dc4092b7ebab9528d09e1df92177ef6e29f9890e
AmoldeepSingh <amoldeepdhillon26@gmail.com>
2023-09-18 01:17:13 +0530

    Add first string code and string folder which will contain all other string concept questions

---
+++ Strings/src/amol/leetcode/Strings.java
@@ -0,0 +1,13 @@
+package amol.leetcode;
+
+public class Strings {
+    public static void main(String[] args) {
+        String s1="Amoldeep";
+        char[] s={'M','O','L','A'};
+        String s2 = new String(s);
+        String s3 = new String("IN CHITKARA UNIVERSITY");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
